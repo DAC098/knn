77ec010a4f3b9adb82dade589e15834304f186a7
dac098 <me@dac098.me>
2025-12-10 00:42:50 -0800

    predict subcommand
    moved the current knn code into its own subcommand to start working on
    the hyper parameter search command. added in a small dataset to test
    with.

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,11 @@ use std::path::PathBuf;
 use std::str::FromStr;
 
 use anyhow::{Context, Error, bail};
-use clap::{Parser, ValueEnum};
-use csv::{ReaderBuilder, StringRecord};
+use clap::{Parser, ValueEnum, Subcommand, Args};
+use csv::{Reader, ReaderBuilder, StringRecord};
 
 fn main() -> anyhow::Result<()> {
     let args = CliArgs::parse();
-
-    if args.columns.is_empty() {
-        bail!("no columns specified to pull numeric data from");
-    }
 
     let result = OpenOptions::new().read(true).open(&args.file);
 
@@ -26,69 +22,39 @@ fn main() -> anyhow::Result<()> {
         },
     };
 
-    // store a reference to the distance algorithm
-    let algo = match args.algo {
-        AlgoType::Euclidean => euclidean_distance,
-        AlgoType::Manhattan => manhattan_distance,
-    };
-
-    let mut reader = ReaderBuilder::new()
+    let reader = ReaderBuilder::new()
         .has_headers(!args.no_header)
         .from_reader(BufReader::new(file));
 
-    // retrieve the label and datapoint columns from the csv reader
-    let (label, columns) = get_columns_and_label(&mut reader, &args.label, &args.columns)?;
-    // parse the provided datapoint to estimate. will expect a similar amount
-    // of numbers as the provided number of columns
-    let datapoint = parse_datapoint(&args.datapoint, columns.len())?;
-    // map the csv records iterator into a list of knn records to use later
-    let iter = reader
-        .records()
-        .enumerate()
-        .map(|(index, maybe)| match maybe {
-            Ok(record) => map_record(label, &columns, index, record),
-            Err(err) => Err(anyhow::Error::new(err)
-                .context(format!("failed to parse csv record. row: {index}"))),
-        });
-
-    // collect all the records since we are offering the ability to run k over
-    // a range vs a single iteration
-    let records: Vec<KnnRecord> = {
-        let mut rtn = Vec::new();
-
-        for maybe in iter {
-            rtn.push(maybe?);
-        }
-
-        rtn
-    };
-
-    // k will be the min of the specified high value or the total number of
-    // records
-    for k in args.k.get_range(records.len()) {
-        let (min, groups) = classify_datapoint(k, &records, algo, &datapoint);
-
-        print!("k value: {k}");
-
-        for v in &datapoint {
-            print!(" {v}");
-        }
-
-        println!();
-
-        for (key, count) in groups {
-            // print the calculated percentage for each group found
-            println!("  {key}: {count} {:.2}", (count as f64) / (min as f64));
-        }
+    match args.cmd {
+        KnnCmd::Predict(arg) => knn_predict(reader, arg),
     }
-
-    Ok(())
 }
 
 /// a simple k nearest neighbors (knn) calculator that loads a csv file
 /// containing records to use for estimating a given datapoint.
 #[derive(Debug, Parser)]
 struct CliArgs {
+    /// indicates that the csv contains no header row
+    #[arg(long)]
+    no_header: bool,
+
+    /// path to the csv file to load
+    #[arg(short, long)]
+    file: PathBuf,
+
+    #[command(subcommand)]
+    cmd: KnnCmd,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum KnnCmd {
+    /// attempts to predict a specific datapoint with the specified dataset
+    Predict(PredictArgs),
+}
+
+#[derive(Debug, Args)]
+pub struct PredictArgs {
     /// the number of neighbors to lookup
     #[arg(short, default_value = "3")]
     k: KValue,
@@ -101,10 +67,6 @@ struct CliArgs {
     #[arg(short, long = "col")]
     columns: Vec<ColumnType>,
 
-    /// indicates that the csv contains no header row
-    #[arg(long)]
-    no_header: bool,
-
     /// the column to use as the label
     #[arg(long)]
     label: ColumnType,
@@ -113,9 +75,6 @@ struct CliArgs {
     #[arg(long)]
     datapoint: String,
 
-    /// path to the csv file to load
-    #[arg(short, long)]
-    file: PathBuf,
 }
 
 /// represents the k value to use for calculations
@@ -237,10 +196,12 @@ where
     let mut columns = Vec::with_capacity(retrieve.len());
 
     let found = if reader.has_headers() {
-        let headers = reader
+        let known_headers = reader
             .headers()
-            .context("failed to retrieve csv headers")?
-            .into_iter()
+            .context("failed to retrieve csv headers")?;
+
+        let headers = known_headers
+            .iter()
             .enumerate()
             .map(|(index, name)| (name, index))
             .collect::<HashMap<&str, usize>>();
@@ -249,7 +210,7 @@ where
             match to_get {
                 ColumnType::Name(name) => {
                     let Some(index) = headers.get(name.as_str()) else {
-                        bail!("unknown column header specified. column: {name}");
+                        bail!("unknown column header specified. column: {name}\navail: {:#?}", headers);
                     };
 
                     columns.push(*index);
@@ -267,7 +228,7 @@ where
         match label {
             ColumnType::Name(name) => {
                 let Some(index) = headers.get(name.as_str()) else {
-                    bail!("unknown label column header specified. column: {name}");
+                    bail!("unknown label column header specified. column: {name}\navail: {:#?}", headers);
                 };
 
                 *index
@@ -415,4 +376,67 @@ where
     }
 
     (min, groups)
+}
+
+fn knn_predict<R>(mut reader: Reader<R>, arg: PredictArgs) -> anyhow::Result<()>
+where
+    R: std::io::Read
+{
+    if arg.columns.is_empty() {
+        bail!("no columns specified to pull numeric data from");
+    }
+
+    // store a reference to the distance algorithm
+    let algo = match arg.algo {
+        AlgoType::Euclidean => euclidean_distance,
+        AlgoType::Manhattan => manhattan_distance,
+    };
+
+    // retrieve the label and datapoint columns from the csv reader
+    let (label, columns) = get_columns_and_label(&mut reader, &arg.label, &arg.columns)?;
+    // parse the provided datapoint to estimate. will expect a similar amount of
+    // numbers as the provided number of columns
+    let datapoint = parse_datapoint(&arg.datapoint, columns.len())?;
+    // map the csv records iterator into a list of knn records to use later
+    let iter = reader
+        .records()
+        .enumerate()
+        .map(|(index, maybe)| match maybe {
+            Ok(record) => map_record(label, &columns, index, record),
+            Err(err) => Err(anyhow::Error::new(err)
+                .context(format!("failed to parse csv record. row: {index}"))),
+        });
+
+    // collect all the records since we are offering the ability to run k over
+    // a range vs a single iteration
+    let records: Vec<KnnRecord> = {
+        let mut rtn = Vec::new();
+
+        for maybe in iter {
+            rtn.push(maybe?);
+        }
+
+        rtn
+    };
+
+    // k will be the min of the specified high value or the total number of
+    // records
+    for k in arg.k.get_range(records.len()) {
+        let (min, groups) = classify_datapoint(k, &records, algo, &datapoint);
+
+        print!("k value: {k} |");
+
+        for v in &datapoint {
+            print!(" {v}");
+        }
+
+        println!();
+
+        for (key, count) in groups {
+            // print the calculated percentage for each group found
+            println!("  {key}: {count} {:.2}", (count as f64) / (min as f64));
+        }
+    }
+
+    Ok(())
 }
