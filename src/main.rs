908b8ebf1a77ad3e2edd2dcec75cf28f924741c7
dac098 <me@dac098.me>
2025-12-10 21:55:09 -0800

    started working on search command

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,7 @@ fn main() -> anyhow::Result<()> {
 
     match args.cmd {
         KnnCmd::Predict(arg) => knn_predict(reader, arg),
+        KnnCmd::Search(arg) => knn_search(reader, arg),
     }
 }
 
@@ -55,6 +56,8 @@ struct CliArgs {
 pub enum KnnCmd {
     /// attempts to predict a specific datapoint with the specified dataset
     Predict(PredictArgs),
+    /// searches for an optimal set of arguments to predict values with
+    Search(SearchArgs),
 }
 
 #[derive(Debug, Args)]
@@ -80,9 +83,37 @@ pub struct PredictArgs {
     datapoint: Datapoint,
 }
 
+#[derive(Debug, Args)]
+pub struct SearchArgs {
+    /// the number of neighbors to lookup
+    #[arg(short, default_value = "3-10")]
+    k: KValue,
+
+    /// specifies the algorithm to use when calculating distances
+    #[arg(long, default_value = "euclidean")]
+    algo: AlgoType,
+
+    /// the list of columns to use when searching
+    #[arg(short, long = "col")]
+    columns: Vec<ColumnType>,
+
+    /// the cross validation value to use
+    #[arg(long, default_value = "3")]
+    cv: usize,
+
+    /// the percent of data to test against
+    #[arg(long, default_value = "0.25")]
+    test: f64,
+
+    /// the colume to use as the label
+    #[arg(long)]
+    label: ColumnType,
+}
+
 /// represents the data collected from the csv for the knn
 #[derive(Debug)]
 pub struct KnnRecord {
+    cols: Vec<usize>,
     data: Vec<f64>,
     label: String,
 }
@@ -203,6 +234,7 @@ fn map_record(
     };
 
     Ok(KnnRecord {
+        cols: columns.to_owned(),
         data: rtn,
         label: found.to_owned(),
     })
@@ -248,6 +280,31 @@ where
     (min, groups)
 }
 
+fn collect_records<R>(mut reader: Reader<R>, label: usize, columns: &[usize]) -> anyhow::Result<Vec<KnnRecord>>
+where
+    R: std::io::Read
+{
+    // map the csv records iterator into a list of knn records to use later
+    let iter = reader
+        .records()
+        .enumerate()
+        .map(|(index, maybe)| match maybe {
+            Ok(record) => map_record(label, &columns, index, record),
+            Err(err) => Err(anyhow::Error::new(err)
+                .context(format!("failed to parse csv record. row: {index}"))),
+        });
+
+    // collect all the records since we are offering the ability to run k over
+    // a range vs a single iteration
+    let mut rtn = Vec::new();
+
+    for maybe in iter {
+        rtn.push(maybe?);
+    }
+
+    Ok(rtn)
+}
+
 fn knn_predict<R>(mut reader: Reader<R>, arg: PredictArgs) -> anyhow::Result<()>
 where
     R: std::io::Read,
@@ -272,27 +329,7 @@ where
         bail!("number of datapoints does not match number of columns");
     }
 
-    // map the csv records iterator into a list of knn records to use later
-    let iter = reader
-        .records()
-        .enumerate()
-        .map(|(index, maybe)| match maybe {
-            Ok(record) => map_record(label, &columns, index, record),
-            Err(err) => Err(anyhow::Error::new(err)
-                .context(format!("failed to parse csv record. row: {index}"))),
-        });
-
-    // collect all the records since we are offering the ability to run k over
-    // a range vs a single iteration
-    let records: Vec<KnnRecord> = {
-        let mut rtn = Vec::new();
-
-        for maybe in iter {
-            rtn.push(maybe?);
-        }
-
-        rtn
-    };
+    let records = collect_records(reader, label, &columns)?;
 
     // k will be the min of the specified high value or the total number of
     // records
@@ -311,6 +348,61 @@ where
             // print the calculated percentage for each group found
             println!("  {key}: {count} {:.2}", (count as f64) / (min as f64));
         }
+    }
+
+    Ok(())
+}
+
+fn split_dataset<'a>(records: &'a [KnnRecord], split: f64) -> (Vec<&'a KnnRecord>, Vec<&'a KnnRecord>) {
+    let mut groups: HashMap<&'a str, Vec<&KnnRecord>> = HashMap::new();
+
+    for record in records {
+        groups
+            .entry(record.label.as_str())
+            // increment if the group was previously added
+            .and_modify(|list| list.push(record))
+            // insert if not already existing
+            .or_insert(vec![record]);
+    }
+
+    let mut train = Vec::new();
+    let mut test = Vec::new();
+
+    for (_, mut records) in groups {
+        let amount = (records.len() as f64 * split).floor() as usize;
+
+        train.extend(records.split_off(amount));
+        test.extend(records);
+    }
+
+    (train, test)
+}
+
+fn knn_search<R>(mut reader: Reader<R>, arg: SearchArgs) -> anyhow::Result<()>
+where
+    R: std::io::Read,
+{
+    if arg.columns.is_empty() {
+        bail!("no columns specified to pull numeric data from");
+    }
+
+    // store a reference to the distance algorithm
+    let algo = match arg.algo {
+        AlgoType::Euclidean => distance::euclidean,
+        AlgoType::Manhattan => distance::manhattan,
+    };
+
+    // retrieve the label and datapoint columns from the csv reader
+    let (label, columns) = get_columns_and_label(&mut reader, &arg.label, &arg.columns)?;
+    let records = collect_records(reader, label, &columns)?;
+
+    let (train, test) = split_dataset(&records, arg.test);
+
+    let selected: Vec<usize> = Vec::new();
+    let avail = columns.to_owned();
+
+    while !avail.is_empty() {
+        let best = None::<usize>;
     }
 
     Ok(())
